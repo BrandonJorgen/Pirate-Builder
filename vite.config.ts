9119f3c384f2a7b7aa130e78cf595a7595c267ab
Brandon Jorgensen <Brandonjorgen@gmail.com>
2024-06-01 19:12:13 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pirate-Builder",
+  base: "/Pirate-Builder/",
   plugins: [react()],
 })
